1556ada0c7f916f6b26cf633f19680ec2227a427
pmcneil <peter@nerderg.com>
2019-04-08 16:58:01 +1000

    NSL-1622 update script update

--- web-app/sql/update-to-32.sql
+++ web-app/sql/update-to-32.sql
@@ -1,48 +1,51 @@
-drop table distribution;
+drop table if exists distribution;
 
-create table dist_entry (
-                            id int8 default nextval('nsl_global_seq') not null,
-                            lock_version int8 default 0 not null,
-                            region_id int8 not null,
-                            tree_element_id int8 not null,
-                            primary key (id)
+drop table if exists dist_entry_dist_status;
+drop table if exists dist_status_dist_status;
+drop table if exists dist_enty;
+drop table if exists dist_region;
+drop table if exists dist_status;
+
+create table dist_entry
+(
+    id              int8 default nextval('nsl_global_seq') not null,
+    lock_version    int8 default 0                         not null,
+    region_id       int8                                   not null,
+    tree_element_id int8                                   not null,
+    primary key (id)
 );
 
-create table dist_entry_dist_status (
-                                        dist_entry_status_id int8,
-                                        dist_status_id int8
+create table dist_entry_dist_status
+(
+    dist_entry_status_id int8,
+    dist_status_id       int8
 );
 
-create table dist_region (
-                             id int8 default nextval('nsl_global_seq') not null,
-                             lock_version int8 default 0 not null,
-                             deprecated boolean default false not null,
-                             description_html text,
-                             display boolean default true not null,
-                             name varchar(255) not null,
-                             primary key (id)
+create table dist_region
+(
+    id               int8    default nextval('nsl_global_seq') not null,
+    lock_version     int8    default 0                         not null,
+    deprecated       boolean default false                     not null,
+    description_html text,
+    name             varchar(255)                              not null,
+    primary key (id)
 );
 
-create table dist_status (
-                             id int8 default nextval('nsl_global_seq') not null,
-                             lock_version int8 default 0 not null,
-                             deprecated boolean default false not null,
-                             description_html text,
-                             display boolean default true not null,
-                             name varchar(255) not null,
-                             primary key (id)
+create table dist_status
+(
+    id               int8    default nextval('nsl_global_seq') not null,
+    lock_version     int8    default 0                         not null,
+    deprecated       boolean default false                     not null,
+    description_html text,
+    name             varchar(255)                              not null,
+    primary key (id)
 );
 
-create table dist_status_dist_status (
-                                         dist_status_combining_status_id int8,
-                                         dist_status_id int8
+create table dist_status_dist_status
+(
+    dist_status_combining_status_id int8,
+    dist_status_id                  int8
 );
-
-alter table if exists dist_region
-    add constraint UK_dtx2gm3sr51pk6b0fysp1ij9r  unique (name);
-
-alter table if exists dist_status
-    add constraint UK_l9d1nxtobmh259wkmxkduec09  unique (name);
 
 alter table if exists dist_entry
     add constraint FK_ffleu7615efcrsst8l64wvomw
@@ -74,7 +77,8 @@ alter table if exists dist_status_dist_status
         foreign key (dist_status_combining_status_id)
             references dist_status;
 
-alter table dist_entry add constraint de_unique_region unique (region_id, tree_element_id);
+alter table dist_entry
+    add constraint de_unique_region unique (region_id, tree_element_id);
 
 -- version
 UPDATE db_version
